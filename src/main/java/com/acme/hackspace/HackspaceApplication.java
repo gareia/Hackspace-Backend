e24deb422b11015086644d93ee015148b8959ec7
Grecia Guerrero <greciaguerreroa@gmail.com>
2020-09-11 00:22:42 -0500

    first commit

---
+++ src/main/java/com/acme/hackspace/HackspaceApplication.java
@@ -0,0 +1,13 @@
+package com.acme.hackspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackspaceApplication.class, args);
+	}
+
+}
