a0becf5f21b5c02839168fd682f4a01b025db3ee
Grecia Guerrero <greciaguerreroa@gmail.com>
2020-09-11 22:48:53 -0500

    model modify

---
+++ src/main/java/com/acme/hackspace/model/task.java
@@ -0,0 +1,36 @@
+package com.acme.hackspace.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="tasks")
+@Getter
+@Setter
+public class task {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @NotBlank
+    @Size(max = 50)
+    @Column(unique = true)
+    private String name;
+
+    private Boolean completed;
+
+    private Date createdAt;
+}
