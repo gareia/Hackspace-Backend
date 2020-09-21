856dc1be102689df74d93b0636f89463656817e2
Grecia Guerrero <greciaguerreroa@gmail.com>
2020-09-21 17:32:13 -0500

    fix: property date

--- src/main/java/com/acme/hackspace/model/task.java
+++ src/main/java/com/acme/hackspace/model/task.java
@@ -1,16 +1,25 @@
 package com.acme.hackspace.model;
 
-import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -19,19 +28,23 @@ import lombok.Setter;
 @Table(name="tasks")
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class Task{
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-    @NotNull
     @NotBlank
     @Size(max = 50)
     @Column(unique = true)
     private String name;
 
-    private Boolean completed;
+    
+    private Boolean completed = false;
 
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP) //almacenar fecha y hora
     private Date createdAt;
 }
--- src/main/java/com/acme/hackspace/resource/SaveTaskResource.java
+++ src/main/java/com/acme/hackspace/resource/SaveTaskResource.java
@@ -1,20 +1,20 @@
 package com.acme.hackspace.resource;
 
-import java.util.Date;
 
 import javax.persistence.Column;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SaveTaskResource {
-    @NotNull
+
     @NotBlank
     @Size(max = 50)
     @Column(unique = true)
     private String name;
 
-    private Boolean completed;
-
-    private Date createdAt;
 }
